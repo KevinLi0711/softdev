04faace3b1b45f447d0b1f892c44b64b5e1f69f1
KevinLi0711 <kli30@stuy.edu>
2022-09-27 21:11:58 -0400

    stuff

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -7,47 +7,54 @@ time spent: .75 hours (in-class time)
 DISCO
     - Typecasting something into an int is done by int(<thing you want to typecast>)
     - list() can take a dictionary parameter and return a list of keys
-    - .choice() takes a list parameter and returns a random value in the list
-    - .keys and .values return a view object, and you can't access the values in a view object using an index (ex: krewes.keys()[0] doesn't work)
+    - rng.choice() takes a list parameter and returns a random value in the list
+    - dict.keys and dict.values return a view object, and you can't access the values in a view object using an index (ex: krewes.keys()[0] doesn't work)
 QCC
     - Dictionaries are kinda like 2d arrays except the other dimension can have variables of another data type
 OPS SUMMARY
     Randint:
     - Get a list of the keys in krewes
     - Take the value at a random index from 0 to 2 of the list of keys
-    - Use that key to access the corresponding list and then return the devo at a random index from 0 to 1 less than the length of the list
+    - Use that key to access the corresponding list and then return the devo at a random index from 0 to the length of the list - 1
     Random:
     - Same as above but using .random and typecasting instead of randint
     Choice:
     - use .choice on the list of keys
     - use .choice on the list corresponding to that key
-
 '''
 import random as rng
-krewes = {2:['a','b','c'], 7:['y','z'], 8:['j', 'k', 'l']}
+krewes = {2:["NICHOLAS",  "ANTHONY",  "BRIAN",  "SAMUEL",  "JULIA",  "YUSHA",  "CORINA",  "CRAIG",  "FANG MIN",  "JEFF",  "KONSTANTIN",  "AARON",  "VIVIAN",  "AYMAN",  "TALIA",  "FAIZA",  "ZIYING",  "YUK KWAN",  "DANIEL",  "WEICHEN",  "MAYA",  "ELIZABETH",  "ANDREW",  "VANSH",  "JONATHAN",  "ABID",  "WILLIAM",  "HUI",  "ANSON",  "KEVIN",  "DANIEL",  "IVAN",  "JASMINE",  "JEFFREY"],
+            7:["DIANA",  "DAVID",  "SAM",  "PRATTAY",  "ANNA",  "JING YI",  "ADEN",  "EMERSON",  "RUSSELL",  "JACOB",  "WILLIAM",  "NADA",  "SAMANTHA",  "IAN",  "MARC",  "ANJINI",  "JEREMY",  "LAUREN",  "KEVIN",  "RAVINDRA",  "SADI",  "EMILY",  "GITAE",  "MAY",  "MAHIR",  "VIVIAN",  "GABRIEL",  "BRIANNA",  "JUN HONG",  "JOSEPH",  "MATTHEW",  "JAMES",  "THOMAS",  "NICOLE",  "Karen"],
+            8:["ALEKSANDRA",  "NAKIB",  "AMEER",  "HENRY",  "DONALD",  "YAT LONG",  "SEBASTIAN",  "DAVID",  "YUKI",  "SHAFIUL",  "DANIEL",  "SELENA",  "JOSEPH",  "SHINJI",  "RYAN",  "APRIL",  "ERICA",  "JIAN HONG",  "VERIT",  "JOSHUA",  "WILSON",  "AAHAN",  "GORDON",  "JUSTIN",  "MAYA",  "FAIYAZ",  "SHREYA",  "ERIC",  "JEFFERY",  "BRIAN",  "KEVIN",  "SAMSON",  "BRIAN",  "HARRY",  "wanying"]
+         }
 
 def choose():
     #using randint
-    '''
-    period = list(krewes)[rng.randint(0,2)]
-    listofdevos = krewes[period]
-    devo = listofdevos[rng.randint(0,len(listofdevos) - 1)]
-    '''
+    
+    keys = list(krewes)
+    period = rng.randint(0,len(keys)-1) 
+    period = keys[period] #randomly selects the period
+    devo = rng.randint(0, len(krewes[period])-1) #randomly selects the devo
+    devo = krewes[period][devo]
+    
 
     #using random
     '''
-    period = list(krewes)[int(rng.random() * 3)]
-    listofdevos = krewes[period]
-    devo = listofdevos[int(rng.random() * len(listofdevos))]
+    keys = list(krewes)
+    period = keys[int(rng.random()*2)]
+    classlist = krewes[keys[int(rng.random()*2)]]
+    devo = classlist[int(rng.random()*len(classlist))]
     '''
-    
+
+    '''
     #using choice
-
-    period = rng.choice(list(krewes)) #choose a random key
+    period = rng.choice([2,7,8]) #choose a random key
     devo = rng.choice(krewes[period]) #choose a random index of the list that corresponds with the chosen key
-    
+    '''
 
-    return devo
-    #return rng.choice(krewes[rng.choice([2,7,8])])
+    output = devo + " from period " + str(period)
+    return output
 
 print(choose())
+
+
