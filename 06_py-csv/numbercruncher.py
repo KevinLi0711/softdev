b992f1b3f00dcecdd3185cc09b00617f5dc2aeb8
KevinLi0711 <kli30@stuy.edu>
2022-10-02 16:56:36 -0400

    qcc and stuff

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -1,7 +1,7 @@
-from itertools import count
-from random import random
-from traceback import print_tb
-import random as random
+#from itertools import count
+import random
+#from traceback import print_tb
+#import random as random
 
 
 def choice():
@@ -16,26 +16,18 @@ def choice():
     for x in occupations_file:
         #iterate from the right of the string
         #find the first comma from the right
-        '''
-        for i in range(len(x), 0, -1):
-            if x[i] == ",":
-                index_of_last_comma = i
-                break
-        x = []
-        '''
         x = x.rsplit(",", 1)
-        print(x)
+        #print(x)
 
         dictionary.update({x[0]:x[1]})
     
     #generate a random number 0 and 99.8
     #subtract the value of the percents from that number until the number is <=0
-    #when the number is <=0, retturn the current key value pair
+    #when the number is <=0, return the current key value pair
     number = random.random() * 99.8
     values = list(dictionary.values())
     keys = list(dictionary.keys())
     counter = -1
-    print(values)
     for x in values:
         if number > 0:
             number = number - float(x)
