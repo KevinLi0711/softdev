7e5ff50b153da07a10493ee3445fac9a06a9c32a
KevinLi0711 <kli30@stuy.edu>
2022-11-06 21:17:59 -0500

    cookies are confusing

--- 19_session/app.py
+++ 19_session/app.py
@@ -14,6 +14,9 @@ app = Flask(__name__)    #create Flask object
 # Set the secret key to some random bytes. Keep this really secret!
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
+username = 'karate'
+password = '1234'
+
 '''
 @app.route('/')
 def index():
@@ -25,20 +28,56 @@ def index():
 @app.route('/', methods=['GET', 'POST'])
 def login():
     error = ""
+    form_keys = request.form.keys()
+    print(request.form)
+
+    #Only assign values to session is there are values in request.form to prevent a key error
+    if 'username' in request.form and 'password' in request.form:
+        session['username'] = request.form['username']
+        session['password'] = request.form['password']
+
+    #Checks if the username and password are in session to prevent a key error
+    if 'username' in session and 'password' in session:
+        #Check if the username and password in session match the hardcoded values
+        if session['username'] == username and session['password'] == password:
+            return render_template('response.html', username = session['username'])
+
+        #print an error message on the bottom of the login page depending on what was wrong
+        if not session['password'] == password:
+            error = "incorrect password"
+        if not session['username'] == username:
+            error = "username not found"
+    
+    return render_template('login.html', error_message = error)
+            
+
+
+    '''
     if request.method == 'POST':
         session['username'] = request.form['username']
-        print("username is " + session['username'])
-        print(session.keys())
+        session['password'] = request.form['password']
+        #print(request.form.keys())
+        #print(request.form.values())
+        #print("username is " + session['username'])
+        #print(session.keys())
 
-        if session['username'] == 'karate': #if username is karate, you get shown the response page, otherwise it returns an error page
-            print(session.__dict__)
+        if session['username'] == username and session['password'] == password: #if username is karate, you get shown the response page, otherwise it returns an error page
+            #print(session.__dict__)
+            #print(request.form)
             return render_template('response.html', username = session['username'])
         else:
-            error = "username not found"
+            if not session['username'] == username:
+                error = "username not found"
+            else:
+                if not session['password'] == password:
+                    error = "incorrect password"
         #return app.redirect(app.url_for('index'))
 
     #print(session.__dict__)
+    #print(request.form)
+    #print(request.form.keys())
     return render_template('login.html', error_message = error)
+    '''
     '''
         <form method="post">
             <p><input type=text name=username>
@@ -46,13 +85,15 @@ def login():
         </form>
     '''
 
-@app.route('/logout')
+@app.route('/logout', methods=["GET", "POST"])
 def logout():
-    # remove the username from the session if it's there
-    session.pop('username', None)
+    #Checks if there is a username and password in session before popping to prevent a key error
+    if 'username' in session and 'password' in session:
+        session.pop('username') #removes the username from the session
+        session.pop('password') #removes the password from the session
     #print(session.__dict__)
-    print(session.keys)
-    return app.redirect(app.url_for('login'))
+    #print(session.keys)
+    return app.redirect(app.url_for('login')) #Sends the user back to the login page
 
 
 if __name__ == "__main__": #false if this file imported as module
